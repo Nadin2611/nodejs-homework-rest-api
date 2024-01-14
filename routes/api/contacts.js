a32aaf68181cde47fe8c575f0c70df5b42ca907a
Nadin2611 <132609243+Nadin2611@users.noreply.github.com>
2024-01-14 20:13:47 +0200

    Опис змін, які ви вносите

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,98 @@
-const express = require('express')
+const express = require("express");
+const Joi = require("joi");
 
-const router = express.Router()
+const contacts = require("../../models/contacts");
+const regexName = "^[A-Z][a-z]+ [A-Z][a-z]+$";
+const regexPhone = "^[0-9]{3}-[0-9]{3}-[0-9]{4}$";
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const { HttpError } = require("../../helpers");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const addShema = Joi.object({
+  name: Joi.string().min(3).max(30).pattern(new RegExp(regexName)).required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
+  phone: Joi.string()
+    .min(12)
+    .max(12)
+    .pattern(new RegExp(regexPhone))
+    .required(),
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const allContacts = await contacts.listContacts();
+    res.json(allContacts);
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const contactById = await contacts.getContactById(contactId);
 
-module.exports = router
+    if (!contactById) {
+      throw HttpError(404, "Not found");
+    }
+
+    res.json(contactById);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { error } = addShema.validate(req.body);
+    if (error) {
+      throw HttpError(
+        400,
+        `Missing required ${error.details[0].context.key} field`
+      );
+    }
+    const newContact = await contacts.addContact(req.body);
+    res.status(201).json(newContact);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const deletedContact = await contacts.removeContact(contactId);
+    if (!deletedContact) {
+      throw HttpError(404, "Not found");
+    }
+    res.json({ message: "Contact deleted" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  try {
+    const { error } = addShema.validate(req.body);
+
+    if (error) {
+      console.error(error);
+      throw HttpError(400, `Missing  fields`);
+    }
+
+    const { contactId } = req.params;
+    const updatedContact = await contacts.updateContact(contactId, req.body);
+
+    if (!updatedContact) {
+      throw HttpError(404, "Not found");
+    }
+    res.json(updatedContact);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
