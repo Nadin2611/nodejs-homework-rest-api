59bd113467e5b36ca35e86a1fb5e88a9b7c22583
Nadin2611 <132609243+Nadin2611@users.noreply.github.com>
2024-01-31 12:11:47 +0200

    login

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,24 +1,58 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 import { User } from "../schemas/usersSchemas.js";
 import { HttpError, ctrlWrapper } from "../helpers/index.js";
 
+const { SECRET_KEY } = process.env;
+
 const registerUser = async (req, res) => {
-  const { email: userEmail } = req.body;
-  const user = await User.findOne({ userEmail });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
 
   if (user) {
     throw HttpError(409, "Email in use");
   }
-  const newUser = await User.create(req.body);
-  const { email, subscription } = newUser;
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const { email: userEmail, subscription } = newUser;
 
   res.status(201).json({
     user: {
-      email,
+      email: userEmail,
       subscription,
     },
   });
 };
 
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+
+  const payload = {
+    id: user._id,
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+
+  res.json({
+    token,
+    user: { email, subscription: user.subscription },
+  });
+};
+
 export default {
   registerUser: ctrlWrapper(registerUser),
+  loginUser: ctrlWrapper(loginUser),
 };
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -3,10 +3,12 @@ import { registerSchema } from "../schemas/usersSchemas.js";
 import { validateBody } from "../middlewares/index.js";
 import userControllers from "../controllers/userControllers.js";
 
-const { registerUser } = userControllers;
+const { registerUser, loginUser } = userControllers;
 
 const usersRouter = express.Router();
 
 usersRouter.post("/register", validateBody(registerSchema), registerUser);
+
+usersRouter.post("/login", validateBody(registerSchema), loginUser);
 
 export default usersRouter;
