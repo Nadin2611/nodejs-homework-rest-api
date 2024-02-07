b97c021d658cbf87d81661cb21cfc21b79fbd775
Nadin2611 <132609243+Nadin2611@users.noreply.github.com>
2024-02-07 10:51:51 +0200

    test

--- app.js
+++ app.js
@@ -14,11 +14,13 @@ export const app = express();
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(PORT);
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
     console.log("Database connection successful");
   })
   .catch((error) => {
-    console.log(error.message);
+    console.log("Database connection error:", error.message);
     process.exit(1);
   });
 
--- test/loginUser.test.js
+++ test/loginUser.test.js
@@ -4,8 +4,6 @@ import dotenv from "dotenv";
 dotenv.config();
 import { app } from "../app.js";
 
-// import { loginUser } from "../controllers/userControllers.js";
-
 const { DB_HOST } = process.env;
 
 const ENDPOINT = "/api/users/login";
@@ -19,12 +17,38 @@ describe("test loginUser controller", () => {
     await mongoose.connect(DB_HOST);
   });
   afterAll(async () => {
-    mongoose.disconnect(DB_HOST);
+    await mongoose.disconnect(DB_HOST);
   });
 
   test("loginUser controller return a response with a status code of 200", async () => {
     const response = await request(app).post(ENDPOINT).send(TEST_LOGIN_USER);
 
     expect(response.status).toBe(200);
+  });
+
+  test("loginUser controller return a token", async () => {
+    const response = await request(app).post(ENDPOINT).send(TEST_LOGIN_USER);
+    // console.log(response.body.token);
+    expect(response.body.token).toBeDefined();
+  });
+
+  test("loginUser controller return a user object with email and subscription fields of type String", async () => {
+    const response = await request(app).post(ENDPOINT).send(TEST_LOGIN_USER);
+    expect(response.body.user).toBeDefined();
+
+    // варіант 1     перевірка об'єктів на рівність, чи має об'єкт властивості
+    expect(response.body.user).toEqual(
+      expect.objectContaining({
+        email: expect.any(String),
+        subscription: expect.any(String),
+      })
+    );
+
+    // варіант 2  перевірка чи має об'єкт властивості
+    // expect(response.body.user).toHaveProperty("email", expect.any(String));
+    // expect(response.body.user).toHaveProperty(
+    //   "subscription",
+    //   expect.any(String)
+    // );
   });
 });
